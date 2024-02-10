16505191b1e576b00b0d14d38044e8ad6995d989
unknown <umyt.chariyew@gmail.com>
2024-02-10 06:07:01 -0800

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-      <Product :product="product" v-for="product in productList" :key="product.id" />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-10"><Product @addToCart="cartItems.push($event)" :product="product" v-for="product in productList" :key="product.id" /></div>
+      <div class="col-2">
+        <h3>Sebet</h3>
+        <ul>
+          <li v-for="item in cartItems" :key="item.id">{{ item.title }}</li>
+        </ul>
+        <p v-if="cartItems.length == 0">Sepetiniz bos</p>
+      </div>
+    </div>
+
   </div>
   
 </template>
@@ -35,7 +45,7 @@ export default {
         },
         {
           id: 3,
-          title: "lenovo",
+          title: "Lenovo",
           price: 647,
           count: 102,
           description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit inventore soluta consectetur dolore voluptatibus asperiores!",
@@ -49,7 +59,8 @@ export default {
           description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit inventore soluta consectetur dolore voluptatibus asperiores!",
         
         },
-      ]
+      ],
+      cartItems : []
     }
   }
 }
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -10,6 +10,7 @@
     <li class="list-group-item">Sany: {{ product.count }}</li>
     <li class="list-group-item"> <small>{{product.description}}</small></li>
   </ul>
+  <button type="button" class="btn btn-success">Sebede gos</button>
     </div>
   </div>
   <div class="card my-3 mx-1" style="width: 14rem;">
@@ -21,6 +22,7 @@
     <li class="list-group-item">Sany: {{ product.count }}</li>
     <li class="list-group-item"><small>{{product.description}}</small></li>
   </ul>
+  <button type="button" class="btn btn-success">Sebede gos</button>
     </div>
   </div>
   <div class="card my-3 mx-1" style="width: 14rem;">
@@ -32,6 +34,7 @@
     <li class="list-group-item">Sany: {{ product.count }}</li>
     <li class="list-group-item"><small>{{product.description}}</small></li>
   </ul>
+  <button @click="addToCart(product)" type="button" class="btn btn-success">Sebede gos</button>
     </div>
   </div>
 
@@ -44,6 +47,11 @@
 
 <script>
 export default {
-    props: ["product"]
+    props: ["product"],
+    methods: {
+      addToCart(product){
+      this.$emit("addToCart", product)
+      }
+    }
 }
 </script>
